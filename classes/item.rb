477a449dd2ed9f7a92bd71f05d446a66ff13a4be
Al-Amin <alaminkhanshakil@gmail.com>
2023-11-20 21:04:41 +0600

    Add the boiler plate

--- classes/item.rb
+++ classes/item.rb
@@ -4,15 +4,6 @@ require 'date'
 class Item
   attr_accessor :publish_date, :archived, :genre, :author, :source, :label
 
-  def can_be_archived?
-    return Date.today.year == @publish_date.year > 10
-  end
-
-  def move_to_archive
-    @archived = true
-    @publish_date
-  end
-
   def initialize(publish_date: nil, archived: false)
     @id = SecureRandom.uuid
     @publish_date = publish_date
@@ -20,6 +11,17 @@ class Item
     @author = []
     @source = []
     @label = []
+    @genre = []
+  end
+
+  def move_to_archive
+    return @archived = true if can_be_archived?
+  end
+
+  private
+
+  def can_be_archived?
+    return Date.today.year - @publish_date.year > 10
   end
 
 end
