388cce6fe8f74b41a604d86b3f41ffe6a9494a03
Al-Amin <alaminkhanshakil@gmail.com>
2023-11-21 21:25:58 +0600

    Add test file for book class

--- classes/book.rb
+++ classes/book.rb
@@ -1,8 +1,10 @@
-require 'item'
+require_relative './item'
 
 class Book < Item
+  attr_reader :publish_date, :publisher, :cover_state
+  
   def initialize(publish_date, publisher, cover_state)
-    super(publish_date)
+    super(publish_date: publish_date)
     @publisher = publisher
     @cover_state = cover_state
   end
