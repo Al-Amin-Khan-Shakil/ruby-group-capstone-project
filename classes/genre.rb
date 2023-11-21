fec8d959c758081c0d7764b9e4376df6294a743d
Goldinium <badrbenslama8@gmail.com>
2023-11-21 23:48:46 +0100

    genre and music classes

--- classes/genre.rb
+++ classes/genre.rb
@@ -1,3 +1,11 @@
 class Genre < Item
+    @name = name
 
+    def add_item(item)
+        @item = item
+    end
+
+    private
+    @items = items
+    @id = id
 end
--- classes/music.rb
+++ classes/music.rb
@@ -1,3 +1,9 @@
 class MusicAlbum < Item
-
+    def initialize(on_spotify: nil)
+        @on_spotify = on_spotify
+    end
+    private
+    def can_be_archived()?
+    
+    end
 end
