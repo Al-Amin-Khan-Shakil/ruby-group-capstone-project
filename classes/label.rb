ac3ac86582585ff97ce23edfc77d4d6ce70cad4c
Al-Amin <alaminkhanshakil@gmail.com>
2023-11-22 17:36:11 +0600

    Fixed the test files

---
+++ classes/label.rb
@@ -0,0 +1,19 @@
+require 'securerandom'
+require_relative 'item'
+
+class Label
+  attr_accessor :title, :color
+  attr_reader :id, :items
+
+  def initialize(title, color)
+    @id = SecureRandom.uuid
+    @title = title
+    @color = color
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.label = self
+  end
+end
