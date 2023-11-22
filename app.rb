b252c21a17e0a62ab59b5f0801e7aa1c0b46aae5
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-11-22 19:49:59 +0300

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,5 +1,16 @@
+require_relative 'classes/game'
+require_relative 'classes/author'
+require './modules/author_module'
+require './modules/game_module'
+
 class App
-  def method1
-    puts 'here the main code'
+  include GameModule
+  include AuthorModule
+  attr_reader :labels, :games, :authors
+
+  def initialize
+    @labels = []
+    @games = []
+    @authors = []
   end
 end
