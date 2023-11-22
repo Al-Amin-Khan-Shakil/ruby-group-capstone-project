647daf396b20c3721f888eb88b6c11663998531f
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-11-22 16:44:28 +0300

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,30 +1,49 @@
 require_relative 'app'
 
-class Main
-  def initialize
-    @app = App.new
-    prompt
-  end
+def main
+  app = App.new
+  prompt(app)
+end
 
-  def prompt
-    puts 'Catelogue of things!'
-    loop do
-      # code for user's intake will come here
-    end
-  end
+def prompt(app)
+  puts 'Catelogue of things!'
+  loop do
+    display_options
+    input_number = gets.chomp.to_i
+    break if input_number == 10
 
-  # team, below is possible user's list
-  def display_options
-    puts 'Please enter or input any option [1-10]'
-    puts '1. - List all books'
-    puts '2. - List all music albums'
-    puts '3. - List all games'
-    puts '4. - List all genres'
-    puts '5. - List all labels'
-    puts '6. - List all authors'
-    puts '7. - Add book'
-    puts '8. - Add a music album'
-    puts '9. - Add a game'
-    puts '10. - Exit'
+    actions(input_number, app)
   end
 end
+
+def actions(input_number, app)
+  case input_number
+  when 1
+    app.list_books
+  when 3
+    app.list_games
+  when 5
+    app.list_labels
+  when 7
+    app.add_new_book
+  else
+    puts 'Invalid option, try again'
+  end
+end
+
+# team, below is possible user's list
+def display_options
+  puts 'Please enter or input any option [1-10]'
+  puts '1. - List all books'
+  puts '2. - List all music albums'
+  puts '3. - List all games'
+  puts '4. - List all genres'
+  puts '5. - List all labels'
+  puts '6. - List all authors'
+  puts '7. - Add book'
+  puts '8. - Add a music album'
+  puts '9. - Add a game'
+  puts '10. - Exit'
+end
+
+main
