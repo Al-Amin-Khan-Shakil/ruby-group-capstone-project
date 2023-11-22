6d37ce8cab4cd84f8ea100fc15354a04e3f05282
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-11-22 19:50:44 +0300

    Update game_module.rb

--- modules/game_module.rb
+++ modules/game_module.rb
@@ -9,7 +9,8 @@ module GameModule
       puts "| Publish Date \t\t| Mode \t\t\t| Last Played At \t|"
       puts '-------------------------------------------------------------------------'
       @games.each do |game|
-        puts "| #{game.publish_date} \t\t| #{game.multiplayer ? 'Multiplayer' : 'Singleplayer'} \t\t| #{game.last_played_at} \t\t|"
+        puts "| #{game.publish_date} \t\t| #{game.multiplayer ? 'Multiplayer' : 'Singleplayer'}
+        \t\t| #{game.last_played_at} \t\t|"
         puts '-------------------------------------------------------------------------'
       end
     end
