62c8674573c7505a69905622c18d16676ecd41fe
Al-Amin <alaminkhanshakil@gmail.com>
2023-11-23 19:46:37 +0600

    preserve all data

--- data_manager.rb
+++ data_manager.rb
@@ -9,14 +9,33 @@ class DataManager
     end
   end
 
+  def self.save_label(labels)
+    if labels.any?
+      File.write('./data/labels.json', JSON.dump(labels))
+    else
+      File.write('./date/labels.json', JSON.dump([]))
+    end
+  end
+
   def self.load_books
-    if File.exist?('./data./books.json')
-      books = JSON.parse(File.read('./data/books.json')).map do |book|
+    if File.exist?('./data/books.json')
+      JSON.parse(File.read('./data/books.json')).map do |book|
         Book.new(book['publish_date'], book['publisher'], book['cover_state'])
       end
     else
       File.write('./data/books.json', JSON.dump([]))
+      []
     end
-    books.any? ? books : []
+  end
+
+  def self.load_labels
+    if File.exist?('./data/labels.json')
+      JSON.parse(File.read('./data/labels.json')).map do |label|
+        Label.new(label['title'], label['color'])
+      end
+    else
+      File.write('./data/labels.json', JSON.dump([]))
+      []
+    end
   end
 end
