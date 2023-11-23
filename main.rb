e1a0296c748ce4454a5b4acb29b13e01186e9253
Goldinium <badrbenslama8@gmail.com>
2023-11-23 20:20:49 +0100

    Update main.rb

--- main.rb
+++ main.rb
@@ -11,14 +11,13 @@ def prompt(app)
     display_options
     input_number = gets.chomp.to_i
 
-    if input_number == 10
+    if input_number == 13
       puts "\e[31mThank you for using this app! ♥️ \e[0m"
       break
     end
     if input_number < 7
       actions(input_number, app)
-    else
-      input_number > 6
+    elsif input_number > 6
       actions2(input_number, app)
     end
   end
@@ -46,12 +45,18 @@ end
 def actions2(input_number, app)
   case input_number
   when 7
-    app.add_new_book
+    app.list_movies
   when 8
-    app.add_new_album
+    app.list_source
   when 9
+    app.add_new_book
+  when 10
+    app.add_new_album
+  when 11
     app.add_game
     app.save_data
+  when 12
+    app.add_new_movie
   else
     puts 'Invalid option, try again'
   end
@@ -65,10 +70,13 @@ def display_options
   puts '4. - List all genres'
   puts '5. - List all labels'
   puts '6. - List all authors'
-  puts '7. - Add book'
-  puts '8. - Add a music album'
-  puts '9. - Add a game'
-  puts '10. - Exit'
+  puts '7. - List all movies'
+  puts '8. - List all sources'
+  puts '9. - Add book'
+  puts '10. - Add a music album'
+  puts '11. - Add a game'
+  puts '12. - Add a movie'
+  puts '13. - Exit'
 end
 
 main
