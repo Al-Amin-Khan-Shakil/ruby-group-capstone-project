1206375755ab77fdce4c75100b4116eb0f7d0692
Evans K. Nyamekye <nyamekye2131@gmail.com>
2023-11-23 06:18:09 +0000

    update: corrected movie class to a boolean functionality

--- classes/movie.rb
+++ classes/movie.rb
@@ -4,15 +4,13 @@ require 'date'
 class Movie < Item
   attr_accessor :silet, :publish_date
 
-  # :director #:
-
-  def initialize(silet, publish_date)
-    super(publish_date)
+  def initialize(silet, publish_date, archived: false)
+    super(publish_date, archived: archived)
     @silet = silet
     @publish_date = publish_date
   end
 
   def can_be_archived?
-    super || (@silet == 'bad')
+    super && @silet
   end
 end
