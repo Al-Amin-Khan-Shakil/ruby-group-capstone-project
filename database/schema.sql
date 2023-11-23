6febff3571e9e8e251fbdd2999b6d3d1e04ed0ba
Al-Amin <alaminkhanshakil@gmail.com>
2023-11-23 13:31:57 +0600

    Add sql code for item book label game and authors

--- database/schema.sql
+++ database/schema.sql
@@ -28,3 +28,15 @@ CREATE TABLE labels (
     PRIMARY KEY(id)
 );
 
+CREATE TABLE game (
+    last_played_at DATE,
+    multiplayer BOOLEAN,
+);
+
+CREATE TABLE authors (
+    id  INT GENERATED ALWAYS AS IDENTITY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    PRIMARY KEY(id)
+    FOREIGN KEY(id) REFERENCES item(id)
+);
