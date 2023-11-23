61dd20a068e96d2877cb1d70ae9ab98a2011c727
Al-Amin <alaminkhanshakil@gmail.com>
2023-11-23 19:46:37 +0600

    preserve all data

--- app.rb
+++ app.rb
@@ -1,10 +1,11 @@
 require_relative 'classes/book'
+require_relative 'classes/label'
 require_relative 'data_manager'
 
 class App
   def initialize
     @books = DataManager.load_books
-    @labels = []
+    @labels = DataManager.load_labels
   end
 
   def list_books
@@ -30,15 +31,23 @@ class App
   end
 
   def add_new_book
+    puts 'Title of book: '
+    title = gets.chomp
     puts 'Published date (dd/mm/yy): '
     publish_date = Date.parse(gets.chomp)
     puts 'Publisher: '
     publisher = gets.chomp
+    puts 'Color of cover: '
+    color = gets.chomp
     puts 'Cover-state (good or bad): '
     cover_state = gets.chomp
     new_book = Book.new(publish_date, publisher, cover_state)
+    new_label = Label.new(title, color)
+    new_label.add_item(new_book)
     @books << new_book
+    @labels << new_label
     DataManager.save_book(@books)
+    DataManager.save_label(@labels)
     puts 'Book added successfully'
   end
 end
