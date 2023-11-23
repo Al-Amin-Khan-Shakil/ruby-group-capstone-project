323e8568f16a0d2120af797dcdcada342c1961a4
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-11-23 15:35:39 +0300

    Update app.rb to save data

--- app.rb
+++ app.rb
@@ -2,15 +2,29 @@ require_relative 'classes/game'
 require_relative 'classes/author'
 require './modules/author_module'
 require './modules/game_module'
+require './modules/storage'
+require 'json'
 
 class App
   include GameModule
   include AuthorModule
+  include StorageModule
   attr_reader :labels, :games, :authors
 
   def initialize
+    prep_for_storage
     @labels = []
-    @games = []
-    @authors = []
+    @games = load_games
+    @authors = load_authors
+  end
+
+  def prep_for_storage
+    create_file('games')
+    create_file('authors')
+  end
+
+  def save_data
+    save_to_file(@games.map(&:to_hash), 'games')
+    save_to_file(@authors.map(&:to_hash), 'authors')
   end
 end
