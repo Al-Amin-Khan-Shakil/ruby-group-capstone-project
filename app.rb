b886f87ee1b4a1eb6b4a057e62ebb634aea8a7fb
Goldinium <badrbenslama8@gmail.com>
2023-11-23 16:12:00 +0100

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,11 +1,14 @@
 require_relative 'classes/book'
 require_relative 'classes/label'
 require_relative 'data_manager'
+require_relative 'classes/music'
 
 class App
   def initialize
     @books = DataManager.load_books
     @labels = DataManager.load_labels
+    @album = []
+    @genre = []
   end
 
   def list_books
@@ -50,4 +53,28 @@ class App
     DataManager.save_label(@labels)
     puts 'Book added successfully'
   end
+
+  def list_album
+    if @album.empty?
+      puts 'There are no albums saved currently'
+    else
+      @album.each_with_index do |album, index|
+        puts "#{index}. Album: #{album}"
+      end
+    end
+    puts
+  end
+
+  def add_new_album
+    puts 'Enter a date: DD/MM/YYYY'
+    album_date = gets.chomp
+    puts 'enter Y if it\'s on spotify or N if it\'s not'
+    on_spotify = gets.chomp.downcase == 'y'
+    new_album = MusicAlbum.new(on_spotify:on_spotify, publish_date:album_date)
+    new_album.add_item(new_album)
+    @album << new_album
+
+    puts 'Album added successfully'
+  end
+
 end
